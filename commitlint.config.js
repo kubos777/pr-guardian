61f5c5ed92241ff7f24ec9410fd57b2041669844
kubos777 <jorgech777@icloud.com>
2026-07-21 23:41:57 -0600

    chore: setup commitlint and husky for commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'hotfix', 'docs', 'chore', 'refactor', 'test', 'style'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [1, 'always'],
+  },
+};
